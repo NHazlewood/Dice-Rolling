64e4742efdd790e382cdeb3ee9f13767d3fe3bbe
edward <edward@pdx.edu>
2019-11-13 21:04:17 -0800

    added rolling
    untested / compiled but rolling with color should be written. Next step is testing/code corrections  then displaying dice rolls with this new inforamtion and style.

--- components/DiceInput2.js
+++ components/DiceInput2.js
@@ -10,15 +10,20 @@ class DiceInput extends React.Component {
         this.state = {bonus: 0}
         this.state = {diceType: 0}
         this.state = {numberOfDice: 0}
-        this.state = {diceColor: ''}
+        this.state = {diceColor: 0}
         this.state = {diceList: []}
+        this.state = {hitOrDamage: false}
+        this.state = {outPut: []}
     }
 
     componentWillMount(){
         this.setState({bonus : 0})
         this.setState({diceType : 0})
         this.setState({numberOfDice : 0})
-        this.setState({diceColor : ''})
+        this.setState({hitOrDamage : false})
+        this.setState({diceColor : 0})
+        this.setState({diceList: []})
+        this.setState({outPut: []})
     }
 
     validateState () {
@@ -42,15 +47,47 @@ class DiceInput extends React.Component {
       }
     
       rollDice = () => {
-        if(!this.validateState()){
+        /*if(!this.validateState()){
           return
         }
-        
+        don't need to validate at this step since we validate when adding dice
+        */
+       //getting all the dice rolls
+        //possibleColors = ['White','Red']
+        diceHolding = []
+        damageHolding =[]
+        toHitHolding = []
+        for (i=0;i<diceList.length();++i){
+          //allocating the bonus
+          if(diceList[i][4]) toHitHolding.push([diceList[i][3],diceList[i][2]])
+          else damageHolding.push([diceList[i][3],diceList[i][2]])
+
+          for(j=0;j<diceList[i][1];++j){
+            //rolling and allocating dice
+            temp = Math.ceil(Math.random() * diceList[i][0])
+            if(diceList[i][4]) toHitHolding.push([temp,diceList[i][2]])
+            else damageHolding.push([temp,diceList[i][2]])
+            diceHolding.push(temp, diceList[i][0], diceList[i][2]) // roll , type, color
+          }
+        }
+        colorDamageTotal = [0 , 0]
+        colorToHitTotal = [0 , 0]
+        //getting all the totals based on color
+        for(i=0;i<possibleColors.length;++i){
+          for(j=0;j<damageHolding;++j){
+            colorDamageTotal[damageHolding[j][1]] += damageHolding[j][0]
+          }
+          for(j=0;j<toHitHolding;++j){
+            colorToHitTotal[toHitHolding[j][1]] += toHitHolding[j][0]
+          }
+        }
+
+        this.setState({outPut : [diceHolding,colorDamageTotal,colorToHitTotal]})
       }
 
       addDice = () => {
         //validate state
-        var newSet = [this.state.diceType,this.state.numberOfDice,this.state.diceColor,this.state.bonus]
+        var newSet = [this.state.diceType,this.state.numberOfDice,this.state.diceColor,this.state.bonus, this.state.hitOrDamage]
         var listCopy = this.state.diceList;
         listCopy.push(newSet);
         listCopy.sort((a,b) => {return b[0]-a[0]});
@@ -69,6 +106,10 @@ class DiceInput extends React.Component {
         this.setState({diceColor : newDiceColor})
       }
 
+      updateHitOrDamage = (newBool) => {
+        this.setState({hitOrDamage : newBool})
+      }
+
     render () {
         return(
             <View style = {styles.upper}>
@@ -80,9 +121,9 @@ class DiceInput extends React.Component {
               </Picker>
 
               <Text style={styles.Text}>Color: </Text>        
-              <Picker  style={{width: 170} } selectedValue = {this.state.diceT} onValueChange = {this.updateDiceColor}>
-                <Picker.Item label = "Red" value = 'Red'/>
-                <Picker.Item label = "White" value = 'White'/>
+              <Picker  style={{width: 170} } selectedValue = {this.state.diceColor} onValueChange = {this.updateDiceColor}>
+                <Picker.Item label = "White" value = '0'/>
+                <Picker.Item label = "Red" value = '1'/>
               </Picker>
               
               <Text style={styles.Text}>#: </Text>
@@ -99,7 +140,14 @@ class DiceInput extends React.Component {
                 keyboardType='numeric'
                 maxLength = {3}
                 onChangeText={(bonus) => this.setState({bonus})}
-                />
+              />
+      
+              <Picker  style={{width: 170} } selectedValue = {this.state.hitOrDamage} onValueChange = {this.updateHitOrDamage}>
+                <Picker.Item label = "Damage" value = 'false'/>
+                <Picker.Item label = "To Hit" value = 'true'/>
+              </Picker>
+
+              
               <TouchableHighlight onPress={() => this.addDice()}>
                 <Text>Add</Text>
               </TouchableHighlight> 
